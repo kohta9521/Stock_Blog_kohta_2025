d5db5d199b2d655c59e4e9f7cf0f49fbf1f36c15
k_95.21 <kohta0421kochi@gmail.com>
2024-09-20 13:12:29 +0900

    add new aws amplify default setting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "stockblog": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
