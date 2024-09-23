8587d9a5bab28807464090640b47665426320c95
k_95.21 <kohta0421kochi@gmail.com>
2024-09-23 17:18:35 +0900

    add new sidebar sample text

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0318c2419d90bd61")
+/******/ 		__webpack_require__.h = () => ("865af6e7a72f6e90")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,86 @@ export default function Home() {
               text="Portfolio Site"
               changePage={true}
             />
+            <SideSecTitle
+              id="build"
+              text="Build & connect backend"
+              link="/"
+              onClick={() => {}}
+              isSelected={true}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Authentication"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="API (GraphQL)"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="API (REST)"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Strorage"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Functions"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Server-Side Rendering"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Existing resources"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="utilties"
+              changePage={false}
+            />
+            <SideSecTitle
+              id="build"
+              text="Deploy & host"
+              link="/"
+              onClick={() => {}}
+              isSelected={true}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Amplify hosting"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="deployment"
+              changePage={false}
+            />
+            <SideSecLink
+              id={"aboutpagelink"}
+              href="/about"
+              text="Custom configuration"
+              changePage={false}
+            />
           </SideBar>
         }
         rightChildren={
