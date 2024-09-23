5b0e37ae1242c4c3e26fa875c444501f24bf5bee
k_95.21 <kohta0421kochi@gmail.com>
2024-09-23 21:04:07 +0900

    make new page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("900b30895504dd10")
+/******/ 		__webpack_require__.h = () => ("bab3aca320b1574b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,235 @@
+"use client";
 import React from "react";
 
 // styles
 import styles from "./styles/About.module.scss";
 
 // components
+import Header from "@/components/organisms/Header";
+import ListBlockItem from "@/components/atoms/ListBlockItem";
+import DefaultFlexLayout from "@/components/templates/DefaultFlexLayout";
+import SideBarSecBox from "@/components/molecules/SideBarSecBox";
+import SideSecTitle from "@/components/atoms/SideSecTitle";
+import SideBar from "@/components/organisms/SideBar";
+import SideSecLink from "@/components/atoms/SideSecLink";
+import { Main } from "next/document";
+import Hero from "@/components/organisms/Hero";
 
 const About = () => {
   return (
-    <div className={styles.about}>
-      <div className={styles.container}></div>
-    </div>
+    <>
+      <Header>
+        <ListBlockItem id="contact" text="CONTACT" href="/" />
+      </Header>
+      <DefaultFlexLayout
+        id="blog-page-layout"
+        leftChildren={
+          <SideBar id="topSideBard">
+            {/* about */}
+            <SideBarSecBox id="about">
+              <SideSecTitle
+                id="top"
+                text="TOP"
+                link="/"
+                onClick={() => {}}
+                isSelected={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="About"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Concept"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Service"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Portfolio Site"
+                changePage={true}
+              />
+            </SideBarSecBox>
+            {/* sample */}
+            <SideBarSecBox id="sample">
+              <SideSecTitle
+                id="build"
+                text="Build & connect backend"
+                link="/"
+                onClick={() => {}}
+                isSelected={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Authentication"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="API (GraphQL)"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="API (REST)"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Strorage"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Functions"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Server-Side Rendering"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Existing resources"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="utilties"
+                changePage={false}
+              />
+            </SideBarSecBox>
+            {/* deploy */}
+            <SideBarSecBox id="host">
+              <SideSecTitle
+                id="build"
+                text="Deploy & host"
+                link="/"
+                onClick={() => {}}
+                isSelected={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Amplify hosting"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="deployment"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+            </SideBarSecBox>
+            {/* sample */}
+            <SideBarSecBox id="sample2">
+              <SideSecTitle
+                id="build"
+                text="Git Hub"
+                link="/"
+                onClick={() => {}}
+                isSelected={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Amplify hosting"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="deployment"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+            </SideBarSecBox>
+            {/* sample */}
+            <SideBarSecBox id="sample2">
+              <SideSecTitle
+                id="build"
+                text="Git Hub"
+                link="/"
+                onClick={() => {}}
+                isSelected={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Amplify hosting"
+                changePage={false}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="deployment"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+              <SideSecLink
+                id={"aboutpagelink"}
+                href="/about"
+                text="Custom configuration"
+                changePage={true}
+              />
+            </SideBarSecBox>
+          </SideBar>
+        }
+        rightChildren={<h1>sample</h1>}
+      />
+    </>
   );
 };
 
