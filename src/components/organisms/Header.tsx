160ad2ed355e80f03273caa87b9a51b404cd1f11
k_95.21 <kohta0421kochi@gmail.com>
2024-09-29 23:14:02 +0900

    add new logo components

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// scss
+import styles from "./styles/Header.module.scss";
+
+// components
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/organisms/stories/Header.stories.tsx
+++ src/components/organisms/stories/Header.stories.tsx
@@ -2,11 +2,9 @@ import { Meta, StoryObj } from "@storybook/react";
 
 // components
 import Header from "../Header";
-import ListItem from "@/components/atoms/ListItem";
-import ListBlockItem from "@/components/atoms/ListBlockItem";
 
 const meta: Meta = {
-  title: "Organisms/Header",
+  title: "Components/Organisms/Header",
   component: Header,
   tags: ["autodocs"],
 };
@@ -16,15 +14,5 @@ export default meta;
 type HeaderStory = StoryObj<typeof Header>;
 
 export const Default: HeaderStory = {
-  args: {
-    children: (
-      <>
-        <ListItem id="about" text="ABOUT" href="/about" />
-        <ListItem id="blogList" text="BLOG LIST" href="/bloglist" />
-        <ListItem id="profile" text="PROFILE" href="/profile" />
-        <ListItem id="news" text="NEWS" href="/news" />
-        <ListBlockItem id="contact" text="CONTACT" href="/contact" />
-      </>
-    ),
-  },
+  render: () => <Header />,
 };
