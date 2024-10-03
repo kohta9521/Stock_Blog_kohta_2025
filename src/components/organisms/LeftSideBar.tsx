f94f570a388686cf835ae04538a49c244acd870b
k_95.21 <kohta0421kochi@gmail.com>
2024-10-03 15:13:18 +0900

    add new sidebar components

---
+++ src/components/organisms/LeftSideBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// scss
+import styles from "./styles/LeftSideBar.module.scss";
+
+// components
+
+const LeftSideBar = () => {
+  return <div className={styles.sidebar}></div>;
+};
+
+export default LeftSideBar;
