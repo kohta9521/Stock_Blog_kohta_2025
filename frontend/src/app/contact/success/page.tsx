5d2a279a78a0b870bf06b766e81d515fc46bc589
k_95.21 <kohta0421kochi@gmail.com>
2025-01-20 22:44:27 +0900

    add new contact newt form

---
+++ frontend/src/app/contact/success/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// next
+import Link from "next/link";
+
+const Success = () => {
+  return (
+    <div className="">
+      <h1>success Page</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default Success;
